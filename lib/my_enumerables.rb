ad78cead3998643ef0894b018a7230d2338be5f6
andresg <Andres Gomez>
2022-04-23 22:12:23 -0500

    feat: adding my solutions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -8,4 +8,76 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    for i in 0..(length - 1) do
+      yield(self[i])
+    end
+    self
+  end
+
+  def my_each_with_index
+    for i in 0..(length - 1) do
+      yield(self[i], i)
+    end
+    self
+  end
+
+  def my_select
+    res = []
+    my_each do |e|
+      res.push(e) if yield(e)
+    end
+    res
+  end
+
+  def my_all?
+    res = true
+    my_each do |e|
+      res &&= yield(e)
+    end
+    res
+  end
+
+  def my_any?
+    res = false
+    my_each do |e|
+      res ||= yield(e)
+    end
+    res
+  end
+
+  def my_none?
+    res = true
+    my_each do |e|
+      res &&= !yield(e)
+    end
+    res
+  end
+
+  def my_count
+    if block_given?
+      c = 0
+      my_each do |e|
+        c += 1 if yield(e)
+      end
+      c
+    else
+      length
+    end
+  end
+
+  def my_map
+    res = []
+    my_each do |e|
+      res.push(yield(e))
+    end
+    res
+  end
+
+  def my_inject(res = 0, &block)
+    my_each do |e|
+      res = block.call(res, e)
+    end
+    res
+  end
 end
